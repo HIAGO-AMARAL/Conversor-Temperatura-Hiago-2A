8039972f7a6482edc8eda5b5839d390a81420816
2024105070069 <2024105070069@aqs-lab-c-07.ifro.local>
2025-04-29 11:35:37 -0400

    Primeiro Commint

--- src/main/java/ConversorTemperaturaHiago2A.java
+++ src/main/java/ConversorTemperaturaHiago2A.java
@@ -1,14 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-/**
- *
- * @author 2024105070069
- */
+import java.util.Scanner;
+
 public class ConversorTemperaturaHiago2A {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("===Conversor de temperatura===");
+            System.out.println("1. Celsius");
+            System.out.println("2. Fahrenheit");
+            System.out.println("3. Kelvin");
+            System.out.println("4. Sair");
+            System.out.println("Escolha a unidade de Origem (1-4)");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 4) {
+                continuar = false;
+                continue;
+            }
+            if (escolha < 1 || escolha > 4) {
+                System.out.println("Opcao Invalida");
+                continue;
+            }
+            System.out.println("Digite o valor da temperatura: ");
+            double graus = scanner.nextDouble();
+
+            switch (escolha) {
+                case 1:
+                    celsius celsius = new celsius(graus);
+                    System.out.println("******************");
+                    System.out.println("Resultado: ");
+                    System.out.println("fahrenheit" + celsius.parafahrenheit());
+                    System.out.println("Kelvin" + celsius.paraKelvin());
+                    System.out.println("*******************");
+                    break;
+                case 2:
+                    fahrenheit f = new fahrenheit(graus);
+                    System.out.println("******************");
+                    System.out.println("Resultado: ");
+                    System.out.println("celsius" + f.paracelsius());
+                    System.out.println("Kelvin" + f.paraKelvin());
+                    System.out.println("*******************");
+                    break;
+                case 3:
+                    Kelvin k = new Kelvin(graus);
+                    System.out.println("******************");
+                    System.out.println("Resultado: ");
+                    System.out.println("celsius" + k.paracelsius());
+                    System.out.println("fahrenheit" + k.parafahrenheit());
+                    System.out.println("*******************");
+                    break;
+            }
+        }
+        System.out.println("programa encerrado");
+        scanner.close();
+
     }
+
 }
+     
+     
+    
+
